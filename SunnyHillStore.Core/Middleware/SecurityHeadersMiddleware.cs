0411a385f3157156750285a55aed90db99d9f05c
Artan Bojaxhi <artan.b@gjirafa.com>
2025-02-11 20:34:07 +0100

    revert commented changes

--- SunnyHillStore.Core/Middleware/SecurityHeadersMiddleware.cs
+++ SunnyHillStore.Core/Middleware/SecurityHeadersMiddleware.cs
@@ -12,11 +12,11 @@ public class SecurityHeadersMiddleware
 
     public async Task InvokeAsync(HttpContext context)
     {
-/*        context.Response.Headers.Add("X-Frame-Options", "DENY");
+        context.Response.Headers.Add("X-Frame-Options", "DENY");
         context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
         context.Response.Headers.Add("X-XSS-Protection", "1; mode=block");
-        context.Response.Headers.Add("Content-Security-Policy", 
-            "default-src 'self'; script-src 'self'; style-src 'self'; img-src 'self' data:; font-src 'self';");*/
+        context.Response.Headers.Add("Content-Security-Policy",
+            "default-src 'self'; script-src 'self'; style-src 'self'; img-src 'self' data:; font-src 'self';");
 
         await _next(context);
     }
